b5ca00a14c506d73d5ffa088e8c9c6008494a6a1
James <git@jard.co.uk>
2017-02-03 14:58:37 +0000

    Now outputs the excuse as an object
    Now outputs the excuse as an object, also updated and added some excuses

--- PowerShell/Get-Excuse.ps1
+++ PowerShell/Get-Excuse.ps1
@@ -24,17 +24,23 @@ Function Get-Excuse {
         $Type)
     
     $excuses = @{"My alarm didn't go off this morning, so I overslept." = "alarm";
-        "There must have been a power cut overnight, so my alarm clock didn't go off." = "alarm";
-        "My phone battery must have died overnight, so my alarm didn't go off." = "alarm";
+        "There must've been a power cut overnight, so my alarm clock didn't go off." = "alarm";
+        "My phone battery must've died overnight, so the alarm didn't go off." = "alarm";
         "I'm feeling a bit ill." = "ill";
+        "I've got a bit of a sniff." = "ill";
         "I saw my family over the weekend. They're always ill. Now I'm ill as well." = "ill";
         "Train problems this morning." = "train";
         "A train de-railed at Lewisham earlier this week." = "train";
         "There's been engineering works at London Bridge for the last year." = "train";
         "I checked the rail website and my train's running 10 minutes late today. Stayed in bed." = "train";
+        "Train strike today." = "train";
+        "The trains were delayed when I was going home last night." = "train"
         "I had a drink last night. Overslept." = "hangover";
+        "I had a drink last night. I feel ill today." = "hangover"
         "My boiler's broken." = "home";
         "There's a man coming round to look at my boiler." = "home";
+        "I'm getting a large package delivered." = "home";
+        "I'm using my holiday up (but I haven't booked this in advance). " = "home";
         "It's raining." = "weather";
         "It's snowy." = "weather";
         "It's too hot." = "weather";
@@ -42,9 +48,10 @@ Function Get-Excuse {
         }
     
     If ($Type) {
-        ($Excuses.GetEnumerator() | where {$_.Value -eq $Type}).Name | Get-Random
+        $Excuse = ($Excuses.GetEnumerator() | where {$_.Value -eq $Type}).Name | Get-Random
     }
     Else {
-        $excuses.Keys | Get-Random
+        $Excuse = $excuses.Keys | Get-Random
     }
+    New-Object -TypeName PSCustomObject -Property @{"Excuse" =  $Excuse}
 }
